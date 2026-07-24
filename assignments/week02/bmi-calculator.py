7583163c2a5bd42f04159a489c0b059d64fe66c3
Pirawas Pirapongsin <pirawas.p@ku.th>
2026-07-24 10:59:18 +0700

    Update assignment

--- assignments/week02/bmi-calculator.py
+++ assignments/week02/bmi-calculator.py
@@ -24,9 +24,9 @@ height = float(input("Enter your height(m): "))
 bmi = weight / (height ** 2)
 if bmi < 18.5:
     print("Underweight")
-elif bmi > 25:
+elif bmi < 25:
     print("Normal weight")
-elif bmi > 30:
+elif bmi < 30:
     print("Overweight")
 else:
     print("Obese")
